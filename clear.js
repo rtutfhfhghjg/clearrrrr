3328b0579e8aff13e09f2f1f318724f8b8725aed
rtutfhfhghjg <45015125+rtutfhfhghjg@users.noreply.github.com>
2018-12-07 01:31:08 +0200

    Add files via upload

---
+++ clear.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("message", message => {
+    var prefix = "-";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©KBOOSHDev"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+     client.login('NDYwMTI1MDAzNzc4ODgzNTg1.Dus_Zw.Kv8IPeRCRLepq4Bf5FlUSq-9XQ8')
+
+     
